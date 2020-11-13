bbe4f825fbbd706128b1ea5f53e670cd6ee2317c
vitor muller anunciato <34242474+vitoranunciato@users.noreply.github.com>
2020-11-12 22:58:17 -0600

    trt

--- docs/data/trt_tables.R
+++ docs/data/trt_tables.R
@@ -17,11 +17,15 @@ trt %>%
   kable(escape = F, align = "c",  
         col.names = c("Clethodim (g ai/ha)",
                       "Dicamba (g ea/ha)",
+                      "Non Ionic Surfactant v/v",
+                      "Clethodim (g ai/ha)",
+                      "Dicamba (g ea/ha)",
                       "Non Ionic Surfactant v/v"))%>%
   kable_styling(c("condensed", "hover", "striped"), full_width = F)%>%
   row_spec(0, bold = T, color = "#f5f1e7", background = "#d00000")%>%
-  pack_rows("No NIS*", 1, 7) %>%
-  pack_rows("Add+ NIS*", 8, 14) %>%
-  footnote(general = "All solutions with DRA at 0.5% v/v",
+  pack_rows("No mixture", 1, 4) %>%
+  pack_rows("Mixture", 5, 7) %>%
+  add_header_above(c("No Non Ionic Sufactant (NIS)" = 3, "Non Ionic Sufactant (NIS)" = 3))%>%
+    footnote(general = "All solutions with DRA at 0.5% v/v",
            symbol = c("NIS = Non Ionic Surfactant."))%>%
     save_kable("trt.png", zoom = 3)
